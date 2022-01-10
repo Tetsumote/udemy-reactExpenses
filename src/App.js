6698a1f2c6d8192fef1753f71976f8baab3991eb
Paweł Grajewski <unionproject88@gmail.com>
2022-01-10 19:09:15 +0100

    building a first custom component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./styles.css";
 
+import ExpenseItem from "./components/ExpenseItem";
+
 export default function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox udemy project</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <ExpenseItem />
     </div>
   );
 }
