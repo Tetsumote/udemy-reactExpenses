cd5559b04777e4b50fd263d2a5982755b881e61c
Paweł Grajewski <unionproject88@gmail.com>
2022-01-18 18:21:13 +0100

    add data to state

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -20,16 +20,16 @@ const Expenses = (props) => {
         onChangeFilter={filterChangeHandler}
         selected={filteredYear}
       />
-      <ExpenseItem
-        title={props.expenses[0].title}
-        amount={props.expenses[0].amount}
-        date={props.expenses[0].date}
-      />
-      <ExpenseItem
-        title={props.expenses[1].title}
-        amount={props.expenses[1].amount}
-        date={props.expenses[1].date}
-      />
+      {props.expenses.map((item) => {
+        return (
+          <ExpenseItem
+            key={item.id}
+            title={item.title}
+            amount={item.amount}
+            date={item.date}
+          />
+        );
+      })}
     </Card>
   );
 };
